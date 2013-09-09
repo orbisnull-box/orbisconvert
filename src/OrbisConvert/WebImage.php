a9b29a65f51b48d848901249e5c181ebe36e7964
orbisnull <zen4dev@gmail.com>
2013-09-09 14:03:22 +0900

    add web image converter

---
+++ src/OrbisConvert/WebImage.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * User: orbisnull
+ * Date: 08.09.13
+ */
+
+namespace OrbisConvert;
+
+use OrbisTools\FileConverter;
+
+class WebImage
+{
+    protected $fileConverter;
+
+    /**
+     * @return FileConverter
+     */
+    public function getFileConverter()
+    {
+        if (is_null($this->fileConverter)) {
+            $this->fileConverter = new FileConverter();
+        }
+        return $this->fileConverter;
+    }
+
+    public static function getDimension($file)
+    {
+        $info = getimagesize($file);
+        return ['width' => $info[0], 'height' => $info[1], 'imageType' => $info[2]];
+    }
+
+    function resizeToWidth($fileInput, $fileOutput, $width)
+    {
+        $command = "convert #INPUT# -resize {$width} #OUTPUT#";
+        return $this->getFileConverter()->convert($fileInput, $command, $fileOutput);
+    }
+
+    function resizeToHeight($fileInput, $fileOutput, $height)
+    {
+        $command = "convert #INPUT# -resize x{$height} #OUTPUT#";
+        return $this->getFileConverter()->convert($fileInput, $command, $fileOutput);
+    }
+
+    function scale($fileInput, $fileOutput, $percent)
+    {
+        $command = "convert #INPUT# -resize {$percent}% #OUTPUT#";
+        return $this->getFileConverter()->convert($fileInput, $command, $fileOutput);
+    }
+
+    function resize($fileInput, $fileOutput, $width, $height)
+    {
+        $command = "convert #INPUT# -resize {$width}x{$height} #OUTPUT#";
+        return $this->getFileConverter()->convert($fileInput, $command, $fileOutput);
+    }
+
+    public function resizeToMax($fileInput, $fileOutput, $sideSize)
+    {
+        $command = "convert #INPUT# -resize {$sideSize}x{$sideSize} #OUTPUT#";
+        return $this->getFileConverter()->convert($fileInput, $command, $fileOutput);
+    }
+
+    public function resizeAndScale($fileInput, $fileOutput, $width, $height = null)
+    {
+        $imgSize = self::getDimension($fileInput);
+        $sharpen = ($imgSize['imgType'] === IMAGETYPE_PNG ) ? '' : '-unsharp 0x1';
+        if (($width / $imgSize['width']) >= ($height / $imgSize['height'])) {
+            $command = "convert {$fileInput} -resize {$width}x -gravity center -crop {$width}x{$height} +0+0 {$sharpen} +repage {$fileOutput}";
+        } else {
+            $command = "convert {$fileInput} -resize x{$height} -gravity center -crop {$width}x{$height} +0+0 {$sharpen} +repage {$fileOutput}";
+        }
+        $result =  $this->getFileConverter()->convert($fileInput, $command, $fileOutput);
+        return $result;
+    }
+
+    public function optimize($file)
+    {
+        $imgSize = self::getDimension($file);
+        switch ($imgSize['imageType']) {
+            case IMAGETYPE_JPEG :
+                $command = "jpegoptim --strip-all {$file}";
+                break;
+            case IMAGETYPE_PNG:
+                $command = "optipng  {$file}";
+                break;
+            default :
+                return true;
+        }
+
+        $result =  $this->getFileConverter()->convert($file, $command);
+        return $result;
+    }
+
+}
